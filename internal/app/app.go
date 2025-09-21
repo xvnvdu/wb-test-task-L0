f92fd6187704ef586a502b02a2fa90050f81d404
Ivan <vanzag1738@ya.ru>
2025-09-21 23:10:07 +0300

    Randomly generated orders now saved to db

--- internal/app/app.go
+++ internal/app/app.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"orders/cmd/generator"
+	db "orders/internal/database"
 	"strconv"
 
 	_ "github.com/lib/pq"
@@ -26,15 +27,92 @@ func (a *App) RandomOrdersHandler(w http.ResponseWriter, r *http.Request) {
 	amount, err := strconv.Atoi(value)
 
 	if err != nil {
-		log.Println("Error in internal/app/app.go: line 32:", err)
+		log.Println("Error in internal/app/app.go: line 27:", err)
 		if _, err := w.Write([]byte("Ooops! Something went wrong!\n" +
 			"To generate random order(s), please consider using an INTEGER value.")); err != nil {
 			log.Fatalln("Handler error: RandomOrdersHandler:", err)
 		}
 	} else {
-		order := generator.MakeRandomOrder(amount)
+		orders := generator.MakeRandomOrder(amount)
+		queries := db.New(a.DB)
 
-		orderJSON, err := json.MarshalIndent(order, "", "    ")
+		for _, order := range orders {
+			err := queries.CreateOrder(r.Context(), db.CreateOrderParams{
+				OrderUid:    order.OrderUID,
+				TrackNumber: order.TrackNumber,
+				Entry:       order.Entry,
+				Locale:      order.Locale,
+				InternalSignature: sql.NullString{
+					String: order.InternalSignature,
+					Valid:  order.InternalSignature != "",
+				},
+				CustomerID:      order.CustomerID,
+				DeliveryService: order.DeliveryService,
+				Shardkey:        order.Shardkey,
+				SmID:            int32(order.SmID),
+				DateCreated:     order.DateCreated,
+				OofShard:        order.OofShard,
+			})
+			if err != nil {
+				log.Fatalln("Error inserting order:", err)
+			}
+
+			err = queries.CreateDelivery(r.Context(), db.CreateDeliveryParams{
+				OrderUid: order.OrderUID,
+				Name:     order.Delivery.Name,
+				Phone:    order.Delivery.Phone,
+				Zip:      order.Delivery.Zip,
+				City:     order.Delivery.City,
+				Address:  order.Delivery.Address,
+				Region:   order.Delivery.Region,
+				Email:    order.Delivery.Email,
+			})
+			if err != nil {
+				log.Fatalln("Error inserting delivery:", err)
+			}
+
+			err = queries.CreatePayment(r.Context(), db.CreatePaymentParams{
+				OrderUid:    order.OrderUID,
+				Transaction: order.Payment.Transaction,
+				RequestID: sql.NullString{
+					String: order.Payment.RequestID,
+					Valid:  order.Payment.RequestID != "",
+				},
+				Currency:     order.Payment.Currency,
+				Provider:     order.Payment.Provider,
+				Amount:       int32(order.Payment.Amount),
+				PaymentDt:    int64(order.Payment.PaymentDT),
+				Bank:         order.Payment.Bank,
+				DeliveryCost: int32(order.Payment.DeliveryCost),
+				GoodsTotal:   int32(order.Payment.GoodsTotal),
+				CustomFee:    int32(order.Payment.CustomFee),
+			})
+			if err != nil {
+				log.Fatalln("Error inserting payment:", err)
+			}
+
+			for _, item := range order.Items {
+				err = queries.CreateItem(r.Context(), db.CreateItemParams{
+					OrderUid:    order.OrderUID,
+					ChrtID:      int32(item.ChrtID),
+					TrackNumber: item.TrackNumber,
+					Price:       int32(item.Price),
+					Rid:         item.Rid,
+					Name:        item.Name,
+					Sale:        int32(item.Sale),
+					Size:        item.Size,
+					TotalPrice:  int32(item.TotalPrice),
+					NmID:        int32(item.NmID),
+					Brand:       item.Brand,
+					Status:      int32(item.Status),
+				})
+				if err != nil {
+					log.Fatalln("Error inserting item:", err)
+				}
+			}
+		}
+
+		orderJSON, err := json.MarshalIndent(orders, "", "    ")
 		if err != nil {
 			log.Println("Error marshalling JSON:", err)
 		}
