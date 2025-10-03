ed3d7da0c88f500fe29e0d1f21521b4d6518a804
Ivan <vanzag1738@ya.ru>
2025-10-03 23:12:31 +0300

    SQL prepared for docker: deleted migrations, manual scripts. Rewrote init.sql

--- sql/drop_all.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE orders_db;
-
-DROP OWNED BY orders_user ;
-
-DROP USER orders_user ;
--- sql/init.sql
+++ sql/init.sql
@@ -1,10 +1,60 @@
-CREATE DATABASE orders_db;
-CREATE USER orders_user WITH PASSWORD '12345' ;
+CREATE TABLE IF NOT EXISTS orders (
+    order_uid VARCHAR(255) PRIMARY KEY,
+    track_number VARCHAR(255) NOT NULL,
+    entry VARCHAR(50) NOT NULL,
+    locale VARCHAR(50) NOT NULL,
+    internal_signature VARCHAR(255),
+    customer_id VARCHAR(255) NOT NULL,
+    delivery_service VARCHAR(100) NOT NULL,
+    shardkey VARCHAR(10) NOT NULL,
+    sm_id INTEGER NOT NULL,
+    date_created TIMESTAMPTZ NOT NULL,
+    oof_shard VARCHAR(10) NOT NULL
+);
 
-GRANT ALL PRIVILEGES ON DATABASE orders_db TO orders_user ;
-\c orders_db;
+CREATE TABLE IF NOT EXISTS delivery (
+    order_uid VARCHAR(255) PRIMARY KEY REFERENCES orders (
+        order_uid
+    ) ON DELETE CASCADE,
+    name VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    zip VARCHAR(20) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    address TEXT NOT NULL,
+    region VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL
+);
 
-GRANT ALL PRIVILEGES ON SCHEMA public TO orders_user;
+CREATE TABLE IF NOT EXISTS payments (
+    order_uid VARCHAR(255) PRIMARY KEY REFERENCES orders (
+        order_uid
+    ) ON DELETE CASCADE,
+    transaction VARCHAR(255) NOT NULL,
+    request_id VARCHAR(255),
+    currency VARCHAR(10) NOT NULL,
+    provider VARCHAR(50) NOT NULL,
+    amount INTEGER NOT NULL,
+    payment_dt BIGINT NOT NULL,
+    bank VARCHAR(50) NOT NULL,
+    delivery_cost INTEGER NOT NULL,
+    goods_total INTEGER NOT NULL,
+    custom_fee INTEGER NOT NULL
+);
 
-ALTER DATABASE orders_db OWNER TO orders_user;
-ALTER SCHEMA public OWNER TO orders_user;
+CREATE TABLE IF NOT EXISTS items (
+    item_id SERIAL PRIMARY KEY,
+    order_uid VARCHAR(255) NOT NULL REFERENCES orders (
+        order_uid
+    ) ON DELETE CASCADE,
+    chrt_id INT NOT NULL,
+    track_number VARCHAR(255) NOT NULL,
+    price INT NOT NULL,
+    rid VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    sale INT NOT NULL,
+    size VARCHAR(50) NOT NULL,
+    total_price INT NOT NULL,
+    nm_id INT NOT NULL,
+    brand VARCHAR(50) NOT NULL,
+    status INT NOT NULL
+);
